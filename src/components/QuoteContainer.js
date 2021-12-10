3fcd8b96a10c3f0ff5c42342d3dbc37fdda1160a
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-10 19:20:10 +0100

    proba pobrania pojedynczego cytatu, polaczenie ze zdalnym repozytorium

--- src/components/QuoteContainer.js
+++ src/components/QuoteContainer.js
@@ -10,13 +10,36 @@ const QuoteContainer = (props) => {
   const [quote, setQuote] = useState('')
 
 
+
+  const generateRandomQuote = (array) => {
+    return Math.floor(Math.random() * (array.lenght - 0)) + 0
+  }
+
   const getQuote = () => {
 
     axios.get('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json', {
       mode: 'corse'
     })
     .then(res => {
-      console.log(res);
+      // console.log(res);
+      // console.log(res.data);
+      console.log(res.data[0]);
+
+      
+
+
+
+
+
+
+
+      // const arrQuotes = res.data;
+      // console.log(arrQuotes);
+
+      // let newRandomQuote = generateRandomQuote(arrQuotes);
+      // console.log(newRandomQuote);
+
+
     })
     
   }
