6573773107e6bd63e11c2a1cbbcc00406ef50921
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-10 18:47:39 +0100

    stworzenie layout'u QuoteContainer.js

--- src/components/QuoteContainer.js
+++ src/components/QuoteContainer.js
@@ -8,8 +8,19 @@ const QuoteContainer = (props) => {
 
 
   return (
-    <>
-    </>
+    <section className="quote-main-container">
+      <div className="random-quote-containter">
+        <span id="span-random-quote"></span>
+      </div>
+      <div className="btn-container">
+        <button className="btn-next-quote">
+          Next quote!
+        </button>
+        <button className="btn-container">
+          Previous quote 
+        </button>
+      </div>
+    </section>
   )
 }
 
