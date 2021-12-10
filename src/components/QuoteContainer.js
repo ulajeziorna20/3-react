8abe38dfe3a1bb7a4e25692a47daf09fde5a698a
Ula Jeziorna <jeziornaula@wp.pl>
2021-12-10 18:40:21 +0100

    instalacja aplikacji, stworzenie folderu components razem z importami oraz szkieletem funkcji

---
+++ src/components/QuoteContainer.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import './css/QuoteContainer.css'
+
+
+const QuoteContainer = (props) => {
+
+
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+
+export default QuoteContainer;
